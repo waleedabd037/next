cd3885208be5a023a9e0cb459404686b88aaaa5e
Waleed <waleed.abdullah037@gmail.com>
2025-08-02 13:09:16 +0500

    admin layout adjusted

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth/next";
 import { redirect } from "next/navigation";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/authOptions";
 import { Session } from "next-auth";
 //import "../../globals.css";  // Ensure global styles are imported
 
