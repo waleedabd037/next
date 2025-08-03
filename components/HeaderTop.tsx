b74cccf9c2306ea67f9593d95013c672904af4cb
Waleed <waleed.abdullah037@gmail.com>
2025-08-03 16:43:34 +0500

    headertop cleaned

--- components/HeaderTop.tsx
+++ components/HeaderTop.tsx
@@ -2,170 +2,92 @@
 
 import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
-import React, { useState } from "react";
+import React from "react";
 import toast from "react-hot-toast";
 import { FaRegUser } from "react-icons/fa6";
-import VehicleDropdown from "@/components/VehicleDropdown";
-import ShoppingAssistDropdown from "@/components/ShoppingAssistDropdown";
 
 const HeaderTop = () => {
     const { data: session }: any = useSession();
-    const [isVehicleDropdownOpen, setIsVehicleDropdownOpen] = useState(false);
-    const [isShoppingAssistDropdownOpen, setIsShoppingAssistDropdownOpen] = useState(false);
 
     const handleLogout = () => {
         toast.success("Logout successful!");
         signOut();
     };
 
-    const vehicleCategories = [
-        {
-            name: "SUV / CUV",
-            vehicles: [
-                { name: "Vehicle 1", image: "/vehicle1.jpg" },
-                { name: "Vehicle 2", image: "/vehicle2.jpg" },
-            ],
-        },
-        {
-            name: "Sedan",
-            vehicles: [
-                { name: "Vehicle 3", image: "/k4.png" },
-                { name: "Vehicle 4", image: "/forte.png" },
-            ],
-        },
-    ];
-
-    const shoppingAssistOptions = [
-        { name: "Option 1", description: "Description for option 1" },
-        { name: "Option 2", description: "Description for option 2" },
-        { name: "Option 3", description: "Description for option 3" },
-    ];
-
-    // Toggle Vehicle Dropdown and close Shopping Assist if it's open
-    const toggleVehicleDropdown = () => {
-        if (isShoppingAssistDropdownOpen) {
-            setIsShoppingAssistDropdownOpen(false); // Close Shopping Assist if it's open
-        }
-        setIsVehicleDropdownOpen(!isVehicleDropdownOpen); // Toggle Vehicle Dropdown
-    };
-
-    // Toggle Shopping Assist Dropdown and close Vehicle if it's open
-    const toggleShoppingAssistDropdown = () => {
-        if (isVehicleDropdownOpen) {
-            setIsVehicleDropdownOpen(false); // Close Vehicle if it's open
-        }
-        setIsShoppingAssistDropdownOpen(!isShoppingAssistDropdownOpen); // Toggle Shopping Assist Dropdown
-    };
-
-    // Define the handle functions for both dropdowns
-    const handleVehicleClick = () => {
-        setTimeout(() => {
-            setIsVehicleDropdownOpen(false);
-        }, 1000);
-    };
-
-    const handleShoppingAssistClick = () => {
-        setTimeout(() => {
-            setIsShoppingAssistDropdownOpen(false);
-        }, 1000);
-    };
-
     return (
-        <div>
-            <div className="h-16 text-white bg-[#020617] max-lg:px-5 max-lg:h-16 max-[573px]:px-0 fixed top-0 w-full z-40">
-                <div className="flex justify-between h-full max-lg:flex-col max-lg:justify-center max-lg:items-center max-w-screen-2xl mx-auto px-12 max-[573px]:px-0">
-                    <ul className="flex items-center h-full gap-x-5 max-[370px]:text-sm max-[370px]:gap-x-2">
-                        <li className="flex items-center gap-x-2 font-semibold">
-                            <Link href="/">
-                                <img
-                                    src="/logo v1 svg.svg"
-                                    width={50}
-                                    height={300}
-                                    alt="singitronic logo"
-                                    className="relative right-5 max-[1023px]:w-56"
-                                />
-                            </Link>
-                        </li>
-                        <li
-                           // className="flex items-center gap-x-2 font-semibold relative"
-                            onClick={toggleVehicleDropdown} // Toggle vehicle dropdown
-                        >
-                            <a href="#" className="hover:text-cyan-400">
-                                Vehicles 
-                            </a>
-                        </li>
-                        <li
-                           // className="flex items-center gap-x-2 font-semibold relative"
-                            onClick={toggleShoppingAssistDropdown} // Toggle shopping assist dropdown
-                        >
-                            <a href="#" className="hover:text-cyan-400">
-                            Shopping Assist 
+        <div className="h-16 text-white bg-[#020617] max-lg:px-5 max-lg:h-16 max-[573px]:px-0 fixed top-0 w-full z-40">
+            <div className="flex justify-between h-full max-lg:flex-col max-lg:justify-center max-lg:items-center max-w-screen-2xl mx-auto px-12 max-[573px]:px-0">
+                <ul className="flex items-center h-full gap-x-5 max-[370px]:text-sm max-[370px]:gap-x-2">
+                    <li className="flex items-center gap-x-2 font-semibold">
+                        <Link href="/">
+                            <img
+                                src="/logo v1 svg.svg"
+                                width={50}
+                                height={300}
+                                alt="singitronic logo"
+                                className="relative right-5 max-[1023px]:w-56"
+                            />
+                        </Link>
+                    </li>
+                    <li>
+                        <a href="#" className="hover:text-cyan-400">
+                            Vehicles
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="hover:text-cyan-400">
+                            Shopping Assist
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="hover:text-cyan-400">
+                            Inventory
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="hover:text-cyan-400">
+                            Kia Innovation
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="hover:text-cyan-400">
+                            Owners
+                        </a>
+                    </li>
+                </ul>
 
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" className="hover:text-cyan-400">
-                                Inventory
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" className="hover:text-cyan-400">
-                                Kia Innovation
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" className="hover:text-cyan-400">
-                                Owners
-                            </a>
-                        </li>
-                    </ul>
-
-                    <ul className="flex items-center gap-x-5 h-full max-[370px]:text-sm max-[370px]:gap-x-2 font-semibold">
-                        {!session ? (
-                            <>
-                                <li className="flex items-center">
-                                    <Link href="/login" className="flex items-center gap-x-2 font-semibold">
-                                        <FaRegUser className="text-white" />
-                                        <span>Login</span>
-                                    </Link>
-                                </li>
-                                <li className="flex items-center">
-                                    <Link href="/register" className="flex items-center gap-x-2 font-semibold">
-                                        <FaRegUser className="text-white" />
-                                        <span>Register</span>
-                                    </Link>
-                                </li>
-                            </>
-                        ) : (
-                            <>
-                                <span className="ml-10 text-base">{session.user?.email}</span>
-                                <li className="flex items-center">
-                                    <button
-                                        onClick={handleLogout}
-                                        className="flex items-center gap-x-2 font-semibold"
-                                    >
-                                        <FaRegUser className="text-white" />
-                                        <span>Log out</span>
-                                    </button>
-                                </li>
-                            </>
-                        )}
-                    </ul>
-                </div>
+                <ul className="flex items-center gap-x-5 h-full max-[370px]:text-sm max-[370px]:gap-x-2 font-semibold">
+                    {!session ? (
+                        <>
+                            <li className="flex items-center">
+                                <Link href="/login" className="flex items-center gap-x-2 font-semibold">
+                                    <FaRegUser className="text-white" />
+                                    <span>Login</span>
+                                </Link>
+                            </li>
+                            <li className="flex items-center">
+                                <Link href="/register" className="flex items-center gap-x-2 font-semibold">
+                                    <FaRegUser className="text-white" />
+                                    <span>Register</span>
+                                </Link>
+                            </li>
+                        </>
+                    ) : (
+                        <>
+                            <span className="ml-10 text-base">{session.user?.email}</span>
+                            <li className="flex items-center">
+                                <button
+                                    onClick={handleLogout}
+                                    className="flex items-center gap-x-2 font-semibold"
+                                >
+                                    <FaRegUser className="text-white" />
+                                    <span>Log out</span>
+                                </button>
+                            </li>
+                        </>
+                    )}
+                </ul>
             </div>
-            <VehicleDropdown
-                categories={vehicleCategories}
-                isOpen={isVehicleDropdownOpen}
-                closeDropdown={() => setIsVehicleDropdownOpen(false)}
-                handleVehicleClick={handleVehicleClick} // Pass handleVehicleClick
-            />
-
-            <ShoppingAssistDropdown
-                options={shoppingAssistOptions}
-                isOpen={isShoppingAssistDropdownOpen}
-                closeDropdown={() => setIsShoppingAssistDropdownOpen(false)}
-                handleOptionClick={handleShoppingAssistClick} // Pass handleShoppingAssistClick
-            />
         </div>
     );
 };
